588edd210fdfb34c5fe381c0663841cf239936fd
Joann Pan <joannlpan@gmail.com>
2020-05-17 21:17:42 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -5,6 +5,10 @@ class Song
   attr_accessor :name
   attr_reader :artist
 
+  extend Memorable::ClassMethods
+  extend Findable::ClassMethods
+  include Memorable::InstanceMethods
+
   @@songs = []
 
   def initialize
